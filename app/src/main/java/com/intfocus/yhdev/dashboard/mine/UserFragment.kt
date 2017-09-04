f98fd37b17aaf99acacf5798c5d472f988572add
jameswong <passionatewsj@outlook.com>
2017-09-04 09:54:23 +0800

    fix bugly #384

--- app/src/main/java/com/intfocus/yhdev/dashboard/mine/UserFragment.kt
+++ app/src/main/java/com/intfocus/yhdev/dashboard/mine/UserFragment.kt
@@ -120,7 +120,7 @@ class UserFragment : BaseModeFragment<UserInfoMode>() {
                 .compose(RetrofitUtil.CommonOptions<UserInfoResult>())
                 .subscribe(object : CodeHandledSubscriber<UserInfoResult>() {
                     override fun onError(apiException: ApiException?) {
-                        ToastUtils.show(activity, apiException!!.displayMessage)
+                        ToastUtils.show(ctx, apiException!!.displayMessage)
                     }
 
                     override fun onBusinessNext(mUserInfo: UserInfoResult?) {
@@ -149,7 +149,7 @@ class UserFragment : BaseModeFragment<UserInfoMode>() {
                 .compose(RetrofitUtil.CommonOptions<UserInfoResult>())
                 .subscribe(object : CodeHandledSubscriber<UserInfoResult>() {
                     override fun onError(apiException: ApiException?) {
-                        ToastUtils.show(activity, apiException!!.displayMessage)
+                        ToastUtils.show(ctx, apiException!!.displayMessage)
                     }
 
                     override fun onBusinessNext(mUserInfo: UserInfoResult?) {
