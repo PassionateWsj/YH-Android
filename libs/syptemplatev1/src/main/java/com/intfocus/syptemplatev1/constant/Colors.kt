727d00ed1f39f4e8b6b7247ada6f34f10d3915fe
刘锐麟 <liuruilin@liuruilindeMacBook-Pro.local>
2017-10-10 10:37:46 +0800

    delete: syptemplatev1

--- libs/syptemplatev1/src/main/java/com/intfocus/syptemplatev1/constant/Colors.kt
+++
@@ -1,8 +0,0 @@
-package com.intfocus.syptemplatev1.constant
-
-/**
- * Created by liuruilin on 2017/9/25.
- */
-object Colors {
-    var colorsRGY = intArrayOf(0xffff0000.toInt(), 0xffF4BC45.toInt(), 0xff6aa657.toInt(), 0xffff0000.toInt(), 0xffF4BC45.toInt(), 0xff6aa657.toInt())
-}
